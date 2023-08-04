9074e1ca099dfaa3366facef79ea3c87747f385e
zoe <ZoeOosting@outlook.com>
2023-08-04 15:28:55 +0200

    scroll code boxes

---
+++ components/content/ProseCode.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="outline">
+    <slot />
+  </div>
+
+</template>
+
+<script setup lang="ts">
+defineProps({
+  code: {
+    type: String,
+    default: ''
+  },
+  language: {
+    type: String,
+    default: null
+  },
+  filename: {
+    type: String,
+    default: null
+  },
+  highlights: {
+    type: Array as () => number[],
+    default: () => []
+  },
+  meta: {
+    type: String,
+    default: null
+  }
+})
+</script>
+
+<style scoped>
+.outline {
+  width: 100%;
+  overflow-x: scroll;
+  padding: var(--pad-size);
+}
+</style>
