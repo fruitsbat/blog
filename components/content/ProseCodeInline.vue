00d75d8c114caa9e935d4f5785b332fa7971010d
zoe <ZoeOosting@outlook.com>
2023-08-05 11:06:51 +0200

    inline flex for inline code

--- components/content/ProseCodeInline.vue
+++ components/content/ProseCodeInline.vue
@@ -5,9 +5,25 @@
 <style scoped lang="scss">
 @import "@/assets/scss/shadows.scss";
 
+code {
+  @include text-shadows;
+  padding-left: var(--pad-size-small);
+  padding-right: var(--pad-size-small);
+  display: inline-flex;
+  align-items: center;
+  gap: var(--pad-size-small);
+  color: var(--accent);
+  background-color: var(--black);
+  border-radius: var(--radius);
+  text-decoration: none;
+  text-decoration-color: var(--accent);
+  text-decoration-thickness: var(--border-width);
+  @include text-shadows;
+
+}
+
 @media (prefers-color-scheme: light) {
   code {
-    @include text-shadows;
     background-color: var(--black);
     border-radius: var(--radius);
     padding-left: var(--pad-size-small);
