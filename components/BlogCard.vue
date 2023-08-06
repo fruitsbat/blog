798b0f0d5b520ec2f91bbbcb9cbff2d7394ef210
zoe <ZoeOosting@outlook.com>
2023-08-06 22:05:28 +0200

    dont do the thing on non hover devices

--- components/BlogCard.vue
+++ components/BlogCard.vue
@@ -26,11 +26,11 @@ const { elementX, elementY } = useMouseInElement(target);
 const { width, height } = useElementSize(target);
 
 const computedX = computed(() => {
-  return `${((elementX.value / width.value) - 0.5) * 24}deg`;
+  return `${(elementX.value / width.value - 0.5) * 24}deg`;
 });
 
 const computedY = computed(() => {
-  return `${(((elementY.value / height.value) - 0.5) * 24)}deg`;
+  return `${(elementY.value / height.value - 0.5) * 24}deg`;
 });
 </script>
 
@@ -79,7 +79,7 @@ a:hover .postcard-texture {
   @include animated-background;
   color: var(--white);
   transform: translate3d(0, 0, var(--fs-xl));
-  transition: transform .1s ease-in;
+  transition: transform 0.1s ease-in;
 }
 
 a {
@@ -92,15 +92,19 @@ a {
   transition: filter 1s ease-out;
 }
 
-a:hover .postcard {
-  transform: rotate3d(0, 1, 0, v-bind(computedX)) rotate3d(1, 0, 0, v-bind(computedY));
-  filter: drop-shadow(var(--fs-xl) 0 2em var(--secondary-accent)) drop-shadow(calc(var(--fs-xl) * -1) 0 2em var(--ternary-accent));
-}
-
 .postcard,
 .postcard-overlay,
 .postcard-texture {
   min-height: 320px;
+}
+
+@media (hover: hover) {
+  a:hover .postcard {
+    transform: rotate3d(0, 1, 0, v-bind(computedX))
+      rotate3d(1, 0, 0, v-bind(computedY));
+    filter: drop-shadow(var(--fs-xl) 0 2em var(--secondary-accent))
+      drop-shadow(calc(var(--fs-xl) * -1) 0 2em var(--ternary-accent));
+  }
 }
 
 @media screen and (max-width: 720) {
