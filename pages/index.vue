feeddef640be7b4ad72de293d07a177b96002889
zoe <ZoeOosting@outlook.com>
2023-08-08 14:54:43 +0200

    the SPEHERE

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,26 @@
 <template>
-  <div>
-    <p>welcome to my website</p>
+  <div class="intro-container">
+    <TresCanvas class="model">
+      <TresPerspectiveCamera />
+      <OrbitControls />
+      <TresMesh>
+        <TresSphereGeometry />
+        <TresMeshNormalMaterial />
+      </TresMesh>
+    </TresCanvas>
   </div>
 </template>
 
 <script setup lang="ts">
 definePageMeta({ documentDriven: false });
 </script>
+
+
+<style scoped lang="scss">
+
+.intro-container {
+  width: 100%;
+  height: 100%;
+}
+
+</style>
