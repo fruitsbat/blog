1e75e9895cb44b3f318eb9eb9abad6c734bbcd3d
zoe <ZoeOosting@outlook.com>
2023-08-08 15:00:38 +0200

    the SPEHERE

--- pages/index.vue
+++ pages/index.vue
@@ -9,18 +9,19 @@
       </TresMesh>
     </TresCanvas>
   </div>
+  <p>
+    hehe ksdjfksdjf <br />
+    <br />
+  </p>
 </template>
 
 <script setup lang="ts">
 definePageMeta({ documentDriven: false });
 </script>
 
-
 <style scoped lang="scss">
-
 .intro-container {
   width: 100%;
   height: 100%;
 }
-
 </style>
