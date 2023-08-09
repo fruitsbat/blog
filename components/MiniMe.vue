e5dbf6e5fa2e298bb4e3199987f12874e3a51ddb
zoe <ZoeOosting@outlook.com>
2023-08-09 21:41:34 +0200

    add toon shader

--- components/MiniMe.vue
+++ components/MiniMe.vue
@@ -4,7 +4,7 @@
 
 <script setup lang="ts">
 import * as THREE from "three";
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 </script>
 
 <script lang="ts">
@@ -19,15 +19,13 @@ export default {
     // set up light
     const rightLight = new THREE.PointLight(0xd1aaf0);
     rightLight.position.set(5, 0, 5);
-    rightLight.intensity = 200;
+    rightLight.intensity = 100;
     scene.add(rightLight);
 
     const leftLight = new THREE.PointLight(0xf0d1aa);
     leftLight.position.set(-5, 0, 5);
-    leftLight.intensity = 200;
+    leftLight.intensity = 100;
     scene.add(leftLight);
-
-
 
     function resizeCanvas() {
       const width = canvas?.clientWidth!;
@@ -41,18 +39,18 @@ export default {
     }
 
     var model;
-    loader.load(
-      "/models/minime.gltf",
-      function (gltf) {
-        model = gltf.scene;
-        gltf.scene.rotation.y = 180;
-        scene.add(gltf.scene);
-      }
-    );
+    loader.load("/models/minime.gltf", function (gltf) {
+      model = gltf.scene;
+      gltf.scene.rotation.y = 180;
+      model.traverse((child) => {
+        const c = child as THREE.Mesh;
+        if (c.isMesh) c.material = new THREE.MeshToonMaterial({map: c.map, color:c.material.color});
+      })
+      scene.add(gltf.scene);
+    });
 
     renderer.setPixelRatio(0.1);
     renderer.setClearAlpha(0.0);
-
 
     camera.position.z = 20;
 
@@ -86,6 +84,6 @@ canvas {
   aspect-ratio: 1/1;
   image-rendering: pixelated;
   filter: drop-shadow(var(--fs-xl) 0 2em var(--secondary-accent))
-      drop-shadow(calc(var(--fs-xl) * -1) 0 2em var(--ternary-accent));
+    drop-shadow(calc(var(--fs-xl) * -1) 0 2em var(--ternary-accent));
 }
 </style>
